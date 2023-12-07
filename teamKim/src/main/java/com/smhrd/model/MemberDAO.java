db50d0eb701c25ecde87ac0103f3a492e58bc79f
Hojun <dorajiji9942@gmail.com>
2023-12-07 19:33:06 +0900

    로그인 커밋

---
+++ teamKim/src/main/java/com/smhrd/model/MemberDAO.java
@@ -0,0 +1,63 @@
+package com.smhrd.model;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.database.SqlSessionManager;
+
+public class MemberDAO {
+
+	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+
+	// connection, close, sql문 실행...
+	SqlSession sqlSession = sqlSessionFactory.openSession(true); // 오토 커밋
+
+	public int check(Member vo) {
+		int cnt = 0;
+		try {
+			cnt = sqlSession.insert("com.smhrd.database.MemberMapper.Check", vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return cnt;
+	}
+
+	public int join(Member vo) {
+		int cnt = 0;
+		try {
+			cnt = sqlSession.insert("com.smhrd.database.MemberMapper.Join", vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return cnt;
+	}
+
+	public Member login(String u_id) {
+		Member member = null;
+		try {
+			member = sqlSession.selectOne("com.smhrd.database.MemberMapper.Login", u_id);
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			sqlSession.close();
+		}
+		return member;
+	}
+
+	public int updateMember(Member updateMember) {
+		int cnt = 0;
+		try {
+			cnt = sqlSession.update("com.smhrd.database.MemberMapper.updateMember", updateMember);
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			sqlSession.close();
+		}
+		return cnt;
+	}
+
+}
