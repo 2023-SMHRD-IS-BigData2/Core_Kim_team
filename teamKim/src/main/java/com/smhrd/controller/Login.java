a6b72b98ae16c9ab7299cf7bb6219c6cecc7b777
Hojun <dorajiji9942@gmail.com>
2023-12-07 19:28:26 +0900

    커밋

--- teamKim/src/main/java/com/smhrd/controller/Login.java
+++ teamKim/src/main/java/com/smhrd/controller/Login.java
@@ -7,18 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Login
- */
 @WebServlet(name = "LoginService", urlPatterns = { "/LoginService" })
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
