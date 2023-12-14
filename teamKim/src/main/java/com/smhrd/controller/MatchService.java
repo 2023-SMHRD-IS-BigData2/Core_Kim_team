c4ac24e3ab4941d08816fb0902ec063f3f2dbb66
myoungjoon <myoungjoon0109@gmail.com>
2023-12-14 18:33:05 +0900

    MATCH SERVICE 수정

--- teamKim/src/main/java/com/smhrd/controller/MatchService.java
+++ teamKim/src/main/java/com/smhrd/controller/MatchService.java
@@ -9,9 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.smhrd.model.Match;
+import com.smhrd.model.MatchDAO;
 import com.smhrd.model.Member;
-import com.smhrd.model.Mercenary_Match;
-import com.smhrd.model.Mercenary_MatchDAO;
+
 
 @WebServlet("/MatchService")
 public class MatchService extends HttpServlet {
@@ -21,31 +22,29 @@ public class MatchService extends HttpServlet {
       request.setCharacterEncoding("utf-8");
       HttpSession session = request.getSession();
       
-
       Member vo = (Member)session.getAttribute("vo");
       int mm = Integer.valueOf(request.getParameter("mm"));
       String starttime = request.getParameter("starttime");
       String finishtime = request.getParameter("finishtime");  
-      String peoplenum = request.getParameter("peoplenum");
-      String level = request.getParameter("level");
+      String people_num = request.getParameter("peoplenum");
+      String t_level = request.getParameter("level");
       String comment = request.getParameter("comment");
       String matchDay = request.getParameter("matchDay");
       String place = request.getParameter("place");
    
-      Mercenary_Match mercenary_match = null;
+      Match match = null;
       // 아래부분 개수부족
-      mercenary_match = new Mercenary_Match(mm, matchDay, vo.getU_id(), starttime, finishtime, peoplenum, level);
+      match = new Match(mm, matchDay, vo.getId(), starttime, finishtime, people_num, t_level);
       if (comment != null) {
-         mercenary_match.setWriting(comment);
+         match.setWriting(comment);
       }
+      
       if (place != null) {
-    	  mercenary_match.setPlace(place);
+    	  match.setPlace(place);
     	  int ck = new MatchDAO().makeMEMA(match);
       }else {
     	  int ck = new MatchDAO().makeME(match);
       }
-      
-      
       
       response.sendRedirect("ViewMatch.jsp?date="+matchDay);
    
